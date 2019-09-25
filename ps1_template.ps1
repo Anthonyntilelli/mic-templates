2a57bdbba063bc570f088c08fd4af5aef1b9fd9e
Anthony Tilelli <anthony.n.tilelli@gmail.com>
2019-09-25 17:22:00 +0000

    add basic power shell

---
+++ ps1_template.ps1
@@ -0,0 +1,17 @@
+<#
+  .DESCRIPTION
+   <--DESCRIPTION HERE-->
+  .NOTES
+    Author:
+    LASTEDIT:
+#> 
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version 3
+
+
+# <-- User Functions here-->
+
+
+# <-- Main Code Here -- >
+
+exit
